cc1c0a00ae9dd2ed2e60ee15d7e6eb0fa1f8cb32
Hanusha <u22cs066@coed.svnit.ac.in>
2025-01-27 17:10:01 +0530

    media queries apploed

--- src/components/Skillset/Leetcode.jsx
+++ src/components/Skillset/Leetcode.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
 import { Row } from "react-bootstrap";
+import './Leetcode.css';
 
 const Leetcode = () => {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+    <Row className="leetcode-row">
+      <h1 className="project-heading">
         <strong className="yellow">LeetCode</strong> Stats
       </h1>
-      <ReactMarkdown>
-      ![LeetCode Stats](https://leetcard.jacoblin.cool/Nilesh1012?theme=dark&font=Yeon%20Sung&ext=heatmap)
-      </ReactMarkdown>
+      <img
+        src="https://leetcard.jacoblin.cool/Nilesh1012?theme=dark&font=Yeon%20Sung&ext=heatmap"
+        alt="LeetCode Stats"
+        className="leetcode-image"
+      />
     </Row>
   );
 }
